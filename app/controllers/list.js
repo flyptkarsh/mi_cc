b2d79a967456e1e290db075f19b75c5669f2031d
flyptkarsh <ptkarsh@gmail.com>
2016-06-01 13:22:26 -0400

    added modals

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,28 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isShowingListModal: false,
+  isShowingItemModal: false,
   actions: {
 
     createList: function() {
       var list = this.store.createRecord('list', { title: '', description: ''});
       var item = this.store.createRecord('item', { title: '', description: '', list: list});
+      isShowingListModal: false;
+      isShowingItemModal: false;
     },
     createListItem: function(list) {
       this.store.createRecord('item', { title: '', description: '', list: list});
-    },
-    deleteListItem: function(item) {
-      confirm("Are you sure you want to delete this item?");
-      item.deleteRecord();
-      item.destroyRecord();
     },
     deleteList: function(list) {
       confirm("Are you sure you want to delete this list?");
       list.deleteRecord();
       item.destroyRecord();
 
+    },
+    toggleListModal: function() {
+      this.toggleProperty('isShowingListModal');
+    },
+    toggleItemModal: function() {
+      this.toggleProperty('isShowingItemModal');
     }
-
-
    }
 
 });
